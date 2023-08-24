aef36be51819f0e62e35c610c8129c942ea7a666
VadzimShulepau <shulepau.vadzim@gmail.com>
2023-08-25 01:10:21 +0300

    feat: delete nodemon and mome packages

--- src/js/mailer.js
+++ src/js/mailer.js
@@ -2,43 +2,27 @@
 export default function mailer() {
 
   const feedbackForm = document.querySelector('.form');
-  // feedbackForm.addEventListener('submit', uploadFile);
-  // const file = feedbackForm.file.files[0];
-  feedbackForm.file.addEventListener('change', uploadFile);
+
+  feedbackForm.addEventListener('submit', uploadFile);
 
   async function uploadFile(event) {
     event.preventDefault();
-    const file = event.target.files[0];
+
+    const file = event.target.file.files[0];
     // const formData = new FormData();
-    // formData.append('file', file, file.name);
-    // try {
-    //   const response = await fetch(`/upload/${file.name}`, {
-    //     method: 'POST',
-    //     body: file,
-    //     // headers: {
-    //     //   'Content-Type' : 'application/octate-stream'
-    //     // },
-    //   });
+    // formData.append('file', file.name);
 
-    //   const result = await response.json();
+    try {
+      const response = await fetch(`/upload/${file.name}`, {
+        method: 'POST',
+        body: file,
+        // headers: {
+        //   'Content-Type' : 'application/octate-stream'
+        // },
+      });
 
-    //   console.log(result);
-    //   console.log(response.status)
-    // } catch (error) {
-    //     console.log(error);
-    // };
-
-    const result = new Promise((resolve, reject) => {
-
-      const xhr = new XMLHttpRequest();
-      // xhr.withCredentials = false;
-      xhr.open('POST', `/upload/${file.name}`);
-      // xhr.setRequestHeader('Content-Type', 'application/octate-stream');
-      xhr.onload = resolve;
-      xhr.onerror = reject;
-      xhr.send(file);
-    });
-
-    console.log(await result)
+    } catch (error) {
+      console.log(error);
+    };
   };
 };
